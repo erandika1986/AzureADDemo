c3854f311247d020b16b2d2c4e5a5803b05e814a
Erandika <erandika@solistech.com>
2024-11-04 06:58:16 +0530

    feat: implementation completed

--- AzureADDemoApp/AzureADDemoApp/Program.cs
+++ AzureADDemoApp/AzureADDemoApp/Program.cs
@@ -1,7 +1,29 @@
+using AzureADDemoApp.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AzureADDemoAppContextConnection") ?? throw new InvalidOperationException("Connection string 'AzureADDemoAppContextConnection' not found.");
+
+builder.Services.AddDbContext<AzureADDemoAppContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AzureADDemoAppContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration);
+builder.Services.AddMvc(option =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+
+    option.Filters.Add(new AuthorizeFilter(policy));
+}).AddMicrosoftIdentityUI();
 
 var app = builder.Build();
 
@@ -17,6 +39,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
